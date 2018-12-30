28e8c2913dd57a1dd7680b5a5c344a89665baabe
codestothestars <codestothestars@yahoo.com>
2018-12-29 22:20:43 -0500

    Improve readability in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const bootstrap: () => Promise<NgModuleRef<AppModule>> = async (): Promise<NgModuleRef<AppModule>> =>
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule);
 const logError: (err: unknown) => void = (err: unknown): void => console.error(err); // tslint:disable-line no-console
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+bootstrap()
   .catch(logError);
