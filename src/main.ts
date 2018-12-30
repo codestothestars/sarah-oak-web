84928d059062ce07865a50f117694fd4440018ae
codestothestars <codestothestars@yahoo.com>
2018-12-29 22:15:36 -0500

    Improve readability in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const logError: (err: unknown) => void = (err: unknown): void => console.error(err); // tslint:disable-line no-console
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch((err: unknown) => console.error(err)); // tslint:disable-line no-console
+  .catch(logError);
